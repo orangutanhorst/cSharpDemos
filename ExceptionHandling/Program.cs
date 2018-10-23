b1778d33c45f17ff3c58762076276e69f016fc7f
orangutanklaus <roemmerling@gmail.com>
2018-10-23 10:44:37 +0200

    Exception Handling project added

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var streamReader = new StreamReader(@".\..\..\file.zip");
+            try
+            {
+                //var calculator = new Calculator();
+                //calculator.Divide(5, 0);
+
+                //var content = streamReader.ReadToEnd();
+                //throw new Exception("oops");
+
+                var api = new YouTubeApi();
+                var listVideos = api.GetVideo("Icke32");
+            }
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("You can not divide by zero.");
+            //}
+            //catch (ArithmeticException ex)
+            //{
+
+            //}
+            catch (Exception e)
+            {
+                Console.WriteLine("something happened...");
+                //Console.WriteLine("Sorry, an unexpected error occured.");
+            }
+            finally
+            {
+                if (streamReader != null)
+                    streamReader.Dispose();
+            }
+
+
+            // instead it is also possible to use the using keyword here
+            //try
+            //{
+            //    using (var streamReader = new StreamReader(@".\..\..\file.zip"))
+            //    {
+            //        var content = streamReader.ReadToEnd();
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("sorry, something unexpectated happened...");
+
+            //}
+
+        }
+    }
+}
---
+++ ExceptionHandling/YouTubeApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    public class YouTubeApi
+    {
+        public List<Video> GetVideo(string user)
+        {
+            try
+            {
+                // Access YouTube web service
+                // Read data -xml, parse...
+                // create list of video objects
+                throw new Exception("Oops, again...");
+            }
+            catch (Exception ex)
+            {
+                // Log
+
+                throw new YouTubeException("Could not fetch the video from Youtube.", ex);
+            }
+
+            return new List<Video>();
+        }
+    }
+}
